60f8d80edea8ee4992d54c9cd40e6a0d7ecc47cc
PeteTheMagician <fksdroid@gmail.com>
2014-06-09 22:36:43 +0200

    20140609_2236 working editText field

--- src/pl/dawidfiruzek/snooker/ChoosePlayerActivity.java
+++ src/pl/dawidfiruzek/snooker/ChoosePlayerActivity.java
@@ -1,15 +1,65 @@
 package pl.dawidfiruzek.snooker;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.input.InputManager;
+import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 public class ChoosePlayerActivity extends Activity {
 
+	private EditText playerNameText;
+	private Button buttonOk;
+	private String playerName;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_choose_player);
+		
+		playerNameText = (EditText)findViewById(R.id.editText1);
+		buttonOk = (Button)findViewById(R.id.buttonOk);
+		
+		playerNameText.setOnEditorActionListener(new OnEditorActionListener() {
+			
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				// TODO Auto-generated method stub
+				boolean handled = false;
+//				if((event.getAction() == KeyEvent.ACTION_DOWN) && (actionId == KeyEvent.KEYCODE_ENTER)){
+				if(actionId == EditorInfo.IME_ACTION_DONE){
+					playerName = playerNameText.getText().toString();
+					
+					InputMethodManager inputManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+					inputManager.hideSoftInputFromWindow(playerNameText.getWindowToken(), 0);
+					handled = true;
+				}
+//				}
+				return handled;
+			}
+		});
+		
+		buttonOk.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(playerName != null){
+					buttonOk.setText(playerName);
+				}
+			}
+		});
+		
+		
 	}
 
 	@Override
