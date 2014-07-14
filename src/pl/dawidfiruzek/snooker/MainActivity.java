4fd73b3ad6f098360cb31ac419e88738e945b16e
PeteTheMagician <fksdroid@gmail.com>
2014-07-14 22:47:52 +0200

    20140714_2249 Navigation bar is visible again - easier access to settings

--- src/pl/dawidfiruzek/snooker/MainActivity.java
+++ src/pl/dawidfiruzek/snooker/MainActivity.java
@@ -50,7 +50,6 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 				
-		requestWindowFeature(Window.FEATURE_NO_TITLE);  
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
